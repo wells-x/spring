f6d76def55ca1299be123df93eac98750950ce15
wells_xq <kang991005214@163.com>
2018-09-30 18:29:09 +0800

    s

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,20 +9,21 @@ import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
 /**
- *  guides https://spring.io/guides/gs/spring-boot/
+ * guides https://spring.io/guides/gs/spring-boot/
  */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             System.out.println("Let's inspect the beans provided by Spring Boot:");
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
-            for (String beanName: beanNames){
+            for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
         };
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,20 +1,42 @@
 package hello;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import org.json.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String index() {
+    public String index(String username, String password) {
         System.out.println("Success");
+        System.out.println(username);
+        System.out.println(password);
         return "Greetings from Spring Boot!";
     }
 
-    @PostMapping("/")
-    public String postIndex() {
-        return "Post";
+    @PostMapping(value = "/", produces = "text/html;charset=utf-8")
+    public String postIndex(HttpServletRequest request) throws JSONException, IOException {
+        RestUtils restUtils = new RestUtils();
+        String s = restUtils.getBodyData(request);
+        System.out.println(s);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.valueOf("text/plain;charset='utf-8'"));
+        String username = request.getParameter("data");
+        String password = request.getParameter("password");
+        System.out.println("username is:" + username);
+        System.out.println("password is:" + password);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", 200);
+        jsonObject.put("data", null);
+        return s;
     }
 }
