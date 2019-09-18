c4ef3f5ba3d96cc936cc7911efa75c640d5589dd
wells <kang991005214@163.com>
2019-09-18 17:59:47 +0800

    "注释测试"

--- src/test/java/com/wells/account/controller/LoginControllerTest.java
+++ src/test/java/com/wells/account/controller/LoginControllerTest.java
@@ -10,23 +10,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.AssertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginControllerTest {
 
-//    @Autowired
-//    private UserService userService;
-//
-//    @Test
-//    public void login() throws Exception {
-//        String userName = "wells";
-//        String password = "123456";
-//        User user = userService.findByAccount(userName);
-//        System.out.println(user);
-//        Assert.assertTrue(user.checkPassword(password));
-//    }
-//
-//    @Test
-//    public void index() {
-//    }
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void login() throws Exception {
+        String userName = "wells";
+        String password = "123456";
+        User user = userService.findByAccount(userName);
+        System.out.println(user);
+        Assert.assertTrue(user.checkPassword(password));
+    }
+
+    @Test
+    public void index() {
+    }
 }
